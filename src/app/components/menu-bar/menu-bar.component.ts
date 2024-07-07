445f421c35f9c009b2b8338cacf67fd5549841ab
ErnandesNeponuceno <ernandesmore05@gmail.com>
2024-07-07 20:59:41 -0300

    alteração da estrutura do nav bar, inclusão do menu hamburger, alteração da estrutura do card game, remoção do componente price e label

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -10,6 +10,16 @@ export class MenuBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const hamburger = document.querySelector(".hamburger") as HTMLElement;
+    const navLinks = document.querySelector(".nav-links") as HTMLElement;
+    const links = document.querySelectorAll(".nav-links li") as NodeListOf<HTMLElement>;
 
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle("open");
+      links.forEach(link => {
+        link.classList.toggle("fade");
+      });
+      hamburger.classList.toggle("toggle");
+    });
+  }
 }
