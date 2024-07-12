913360e239a32e7d4ac9a4d47bdd77f9364b7919
ErnandesNeponuceno <ernandesmore05@gmail.com>
2024-07-12 09:55:24 -0300

    melhorias e limpeza do css de cada componente

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  footerSections: { items: string[] }[] = [
+    {
+      items: [
+        'Suporte',
+        'Política de privacidade',
+        'Termos de uso do site',
+        'Mapa do site',
+        'PlayStation Studios',
+        'Legal',
+        'Sobre a SIE'
+      ]
+    },
+    {
+      items: [
+        'Termos de serviço da PSN',
+        'Política de cancelamento da PS Store',
+        'Avisos de saúde',
+        'Sobre as classificações'
+      ]
+    },
+    {
+      items: [
+        'Facebook',
+        'Twitter',
+        'YouTube',
+        'Instagram',
+        'Aplicativo para Android',
+        'Aplicativo para iOS'
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
